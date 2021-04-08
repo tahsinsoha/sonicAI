665a2c772241d0357d3ae20b5eb2796606397e1d
aritra741 <aritra741@gmail.com>
2021-04-08 22:22:21 +0600

    File upload test

--- music/src/App.js
+++ music/src/App.js
@@ -1,25 +1,26 @@
-import React, { useEffect } from "react";
+import React, { Component, useEffect } from "react";
+import fileUpload from "music\src\components"
 import "./App.css";
 
 function App() {
     
-  useEffect(() => {
-    // POST request using fetch inside useEffect React hook
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin':  'http://127.0.0.1:3000',
-        'Access-Control-Allow-Methods': 'POST',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization' },
-        body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-    };
-    fetch('http://localhost:5000/predict', requestOptions)
-        .then(response => console.log(response.json()));
+//   useEffect(() => {
+//     // POST request using fetch inside useEffect React hook
+//     const requestOptions = {
+//         method: 'POST',
+//         Headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+//     };
+//     fetch('http://localhost:5000/predict', requestOptions)
+//         .then(response => console.log(response.json()));
 
-// empty dependency array means this effect will only run once (like componentDidMount in classes)
-}, []);
+// // empty dependency array means this effect will only run once (like componentDidMount in classes)
+// }, []);
 
   return (
-    <div classname="App"/>
+    <div classname="App">
+      <fileUpload/>
+      </div>
       
   );
 }
