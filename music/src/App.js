94f4a8c8695dace145c069e4d3b64c71f8faea4e
Sabiha Tahsin Soha <soha97368@gmail.com>
2021-04-09 14:03:19 +0600

    Genre_prediction done

--- music/src/App.js
+++ music/src/App.js
@@ -1,27 +1,25 @@
 import React, { Component, useEffect } from "react";
-import fileUpload from "music\src\components"
+import FileUpload from "./component/FileUpload";
 import "./App.css";
 
 function App() {
-    
-//   useEffect(() => {
-//     // POST request using fetch inside useEffect React hook
-//     const requestOptions = {
-//         method: 'POST',
-//         Headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-//     };
-//     fetch('http://localhost:5000/predict', requestOptions)
-//         .then(response => console.log(response.json()));
+  //   useEffect(() => {
+  //     // POST request using fetch inside useEffect React hook
+  //     const requestOptions = {
+  //         method: 'POST',
+  //         Headers: {'Content-Type': 'application/json'},
+  //         body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+  //     };
+  //     fetch('http://localhost:5000/predict', requestOptions)
+  //         .then(response => console.log(response.json()));
 
-// // empty dependency array means this effect will only run once (like componentDidMount in classes)
-// }, []);
+  // // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  // }, []);
 
   return (
     <div classname="App">
-      <fileUpload/>
-      </div>
-      
+      <FileUpload />
+    </div>
   );
 }
 
---
+++ music/src/component/FileUpload.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function File() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+  const [answer, setAnswer] = useState("");
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append("file", selectedFile);
+
+    fetch("http://localhost:5000/predict", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+        setAnswer(result.genre);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {isSelected ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+          <p>
+            lastModifiedDate:{" "}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+        <p>{answer}</p>
+      </div>
+    </div>
+  );
+}
+export default File;
