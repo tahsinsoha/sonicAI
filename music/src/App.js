1b0580ab90003cb7cb2faca9fa6116c9a81149d2
Sabiha Tahsin Soha <soha97368@gmail.com>
2021-04-09 14:05:03 +0600

    commit

--- music/src/App.js
+++ music/src/App.js
@@ -3,6 +3,7 @@ import FileUpload from "./component/FileUpload";
 import "./App.css";
 
 function App() {
+<<<<<<< Updated upstream
   //   useEffect(() => {
   //     // POST request using fetch inside useEffect React hook
   //     const requestOptions = {
@@ -12,6 +13,18 @@ function App() {
   //     };
   //     fetch('http://localhost:5000/predict', requestOptions)
   //         .then(response => console.log(response.json()));
+=======
+    
+  useEffect(() => {
+    // POST request using fetch inside useEffect React hook
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({ title: 'React Hooks POST Request Example' }) // js object {title} ke
+    };
+    fetch('http://localhost:5000/predict', requestOptions)
+        .then(response => console.log(response.json()));
+>>>>>>> Stashed changes
 
   // // empty dependency array means this effect will only run once (like componentDidMount in classes)
   // }, []);
